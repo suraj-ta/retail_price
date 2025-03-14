4de8b96000e5a7a1c37ab16ce8d81c00e9f1ba73
vummadisetty.sur@tigeranalytics.com <vummadisetty.sur@tigeranalytics.com>
2025-03-14 14:11:09 +0000

    bug fix in inference.py

--- notebooks/Inference.py
+++ notebooks/Inference.py
@@ -269,8 +269,8 @@ mlclient.log(
 
 ground_truth = gt_df.select([input_table_configs["input_2"]["primary_keys"]] + target_columns).toPandas()
 transformed_features_df = FT_DF.toPandas()
-inference_df = transformed_features_df[[date_column] + feature_columns]
-inference_df = inference_df.rename(columns={date_column: "ds"})
+inference_df = transformed_features_df[feature_columns]
+# inference_df = inference_df.rename(columns={date_column: "ds"})
 display(inference_df)
 
 # COMMAND ----------
